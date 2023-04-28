bc00fafd4bb2abf6dc870f9fc9d2448eceb042d4
IvanKunev <ikunev26@gmail.com>
2023-04-28 14:39:35 +0300

    Basic props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
 import './App.css'
+import { Greet } from './components/Greet'
+import { Person } from './components/Person'
+import { PersonList } from './components/PersonList'
 
 function App() {
+  const personName = {
+    first: 'Ivan',
+    last: 'Kunev'
+  }
+
+  const nameList = [
+    {
+      first: 'Ivan',
+      last: 'Kunev'
+    },
+    {
+      first: 'Maria',
+      last: 'Lazarova'
+    },
+    {
+      first: 'Stoyan',
+      last: 'Georgiev'
+    }
+  ]
   return (
     <div className="App">
+      <Greet name='Kunev' messageCount={10} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   )
 }
